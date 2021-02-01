7b7eaa4553a160e9bdad37b015e8f0534d3d451a
EetuHei <t8heee01@students.oamk.fi>
2021-02-01 15:16:00 +0200

    refactored code to have better structure

--- server/src/index.js
+++ server/src/index.js
@@ -1,60 +1,8 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server');
+const { resolvers } = require('./graphql/resolvers')
+const { typeDefs } = require('./graphql/typeDef')
 
-let persons = [
-    {
-      name: "test test",
-      phone: "040-123543",
-      street: "Kotkantie 1",
-      city: "Oulu",
-      id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-    },
-    {
-      name: "test2 test2",
-      phone: "040-432342",
-      street: "Kotkantie 2",
-      city: "Oulu",
-      id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-    },
-    {
-      name: "test3 test3",
-      street: "Kotkantie 3",
-      city: "Oulu",
-      id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-    },
-  ]
-  
-  const typeDefs = gql`
-    type Person {
-      name: String!
-      phone: String
-      street: String!
-      city: String! 
-      id: ID!
-    }
-  
-    type Query {
-      personCount: Int!
-      allPersons: [Person!]!
-      findPerson(name: String!): Person
-    }
-
-    type User {
-      username: String!
-      id: ID!
-    }
-  `
-
-  const resolvers = {
-    Query: {
-      personCount: () => persons.length,
-      allPersons: () => persons,
-      findPerson: (root, args) =>
-        persons.find(p => p.name === args.name)
-    }
-
-  }
-
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: async ({ req }) => {
