77560c25c77be98a671c095bdaee3a6c4a4a0ca8
EetuHei <t8heee01@students.oamk.fi>
2021-02-01 15:20:51 +0200

    fetch API data, prevent error if args are not given

---
+++ server/src/graphql/dataSource/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+
+const fetchData = async (args) => {
+    const baseURL = process.env.baseURL
+
+    // if args are not given, default args to empty string to fetch baseURL data
+    if (args === undefined) args = ""
+    
+    res = await fetch(`${baseURL}/${args}`)
+    return res.json()
+}
+
+module.exports = {
+    fetchData
+}
