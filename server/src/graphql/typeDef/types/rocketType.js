665db71de4af9501ca321a62d59b8a159c00cd06
EetuHei <t8heee01@students.oamk.fi>
2021-02-01 15:22:48 +0200

    basic gql to query data from the api

---
+++ server/src/graphql/typeDef/types/rocketType.js
@@ -0,0 +1,38 @@
+const {gql} = require('apollo-server')
+
+// define what data you can query for rockets
+const rocketType = gql`
+  type Rocket{
+        id: Int
+        active: Boolean
+        boosters: Int
+        cost_per_launch: Int
+        success_rate_pct: Int
+        first_flight: String
+        country: String
+        company: String
+        rocket_name: String
+        rocket_id: String
+        rocket_type: String
+        engines: Engines!
+        flickr_images: [FlickrImages!]!
+      }
+
+    type Engines{
+        number: Int,
+        type: String
+        version: String
+        layout: String
+        thrust_vacuum: ThrustVacuum!
+    }
+      
+    type ThrustVacuum{
+        kN: Int
+        lbf: Int
+    }
+
+    type FlickrImages{
+        images: String
+    }
+`
+module.exports = {rocketType,}
