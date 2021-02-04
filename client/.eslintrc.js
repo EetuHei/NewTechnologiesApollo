f5d09bbbb8fe4e681e9e4e4eb637a86507fa6f32
EetuHei <t8heee01@students.oamk.fi>
2021-02-04 17:05:59 +0200

    changed eslint to use windows linebreak-style

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   settings: { react: { version: 'detect' } },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
