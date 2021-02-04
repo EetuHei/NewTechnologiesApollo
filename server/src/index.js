ba93701202137309a3af1a4bccfac19145f75746
EetuHei <t8heee01@students.oamk.fi>
2021-02-04 15:56:06 +0200

    removed context since wont be using auth for this project, eslint indentation fixes

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,16 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server')
 const { resolvers } = require('./graphql/resolvers')
 const { typeDefs } = require('./graphql/typeDef')
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    formatError: (err) => {
-      return new Error(`API responded with error code: ${err.extensions.code}`)
-    },
-    context: async ({ req }) => {
-      const auth = req ? req.headers.authorization : null
-      if (auth && auth.toLocaleLowerCase().startsWith('bearer')) {
-        const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-        const currentUser = await User.findById(decodedToken.id)
-        return { currentUser }
-      }
-    },
-  })
-  
-  server.listen().then(({ url, subscriptionsUrl }) => {
-    console.log(`🚀 Server ready at ${url}`)
-    console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`)
-  })
+  typeDefs,
+  resolvers,
+  formatError: (err) => {
+    return new Error(`API responded with error code: ${err.extensions.code}`)
+  },
+})
+
+server.listen().then(({ url, subscriptionsUrl }) => {
+  console.log(`🚀 Server ready at ${url}`)
+  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`)
+})
