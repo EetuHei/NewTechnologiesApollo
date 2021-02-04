b4a185d6feea731e92d9a53661aa850a43fe81ad
RikuHei <43337854+RikuHei@users.noreply.github.com>
2021-02-04 16:45:39 +0200

    added getMissions to resolvers

---
+++ server/src/graphql/resolvers/missions.js
@@ -0,0 +1,12 @@
+const {fetchData} = require("../dataSource/index")
+
+const getMissionsData = async () => {
+    // fetch data from baseURL/missions
+    const res = await fetchData('missions')
+    return res
+}
+
+
+module.exports = {
+    getMissionsData
+}
