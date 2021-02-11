21d7e578eafd68126f4fc5b3d439bf5ce1fb304b
EetuHei <t8heee01@students.oamk.fi>
2021-02-11 11:22:20 +0200

    basic route and some error handling if api is down and localstorage has no data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 import {
   useQuery,
 } from '@apollo/client'
@@ -12,26 +13,36 @@ const App = () => {
 
   useEffect(() => {
     if(result.data){
-      setInitialData(result.data.getRocket)
+      setInitialData(result.data)
       localStorage.setItem('intialData', JSON.stringify(result.data))
     }
-  })
+  }, [])
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
-  if (initalData.length === 0) {
-    console.error('failed to fetch... setting data from local storage')
+  if (!result.loading && initalData.length === 0) {
+    console.error('failed to fetch... setting data from local storage.')
     const dataInStore = JSON.parse(localStorage.getItem('intialData'))
+    if (dataInStore === null) return(<div><h1>Cant display any data at this time, mostlikely issue with API provider.</h1></div>)
     setInitialData(dataInStore)
   }
-
 
   return (
     <div>
       <h1>Space Rockets</h1>
-      <Rocket rockets = {initalData.getRocket}/>
+      <Route exact path="/">
+        {initalData.getRocket ? (<div>
+          <h3>More about rockets</h3>
+          <Link to="/rockets">
+            <img src={initalData.getRocket[0].flickr_images[1]}></img>
+          </Link>
+        </div>) : ('')}
+      </Route>
+      <Switch>
+        <Route exact path="/rockets" render={() => <Rocket rockets = {initalData.getRocket} />} />
+      </Switch>
     </div>
   )
 }
