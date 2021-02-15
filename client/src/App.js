f56d29821a2eb37fa1d98a62c161fcdfa56502d0
RikuHei <43337854+RikuHei@users.noreply.github.com>
2021-02-15 12:37:10 +0200

    added route to access the missions page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route, Link } from 'react-router-dom'
 import {
   useQuery,
 } from '@apollo/client'
-import { ALL_ROCKETS } from './service/queries'
+import { ALL_MISSIONS, ALL_ROCKETS } from './service/queries'
 
 import Rocket from './components/Rocket'
+import Mission from './components/Mission'
 
 const App = () => {
   const result = useQuery(ALL_ROCKETS)
+  const resultMission = useQuery(ALL_MISSIONS)
 
   if (result.loading) {
     return <div>loading...</div>
@@ -30,9 +32,13 @@ const App = () => {
         <Link to="/rockets">
           <img src={result.data.getRocket[0].flickr_images[1]}></img>
         </Link>
+        <Link to="/missions">
+          <h1>Missiones </h1>
+        </Link>
       </Route>
       <Switch>
         <Route exact path="/rockets" render={() => <Rocket rockets = {result.data.getRocket} />} />
+        <Route exact path="/missions" render={() => <Mission missions = {resultMission.data.getMissions} />} />
       </Switch>
     </div>
   )
