8cc61234f405cc2a321d9dabcb405730d22704aa
RikuHei <43337854+RikuHei@users.noreply.github.com>
2021-02-17 13:23:38 +0200

    component for a single mission

--- client/src/components/Mission.js
+++ client/src/components/Mission.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_MISSION_BY_ID } from '../service/queries'
+import { useParams } from 'react-router-dom'
 
-const Mission = ({ missions }) => {
-  console.log(missions, 'missions data')
+const Mission = () => {
+  let { id } = useParams()
+
+  const result = useQuery(GET_MISSION_BY_ID, { variables: { mission_id: id } } )
+
+  if(result.loading)return('Loading...')
+
   return(
     <div>
-      <p>Rocket launch country</p>
-      {missions.map((data) => (
-        <p key={data.id}>{data.mission_name}</p>
-      ))}
+      <h2>{result.data.getMissionDataById.mission_name}</h2>
+      <h3></h3>
     </div>
   )
 }
