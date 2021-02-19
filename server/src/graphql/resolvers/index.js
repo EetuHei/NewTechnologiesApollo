72e7120dc15a53753d5bc0cb7238afb42514c764
EetuHei <t8heee01@students.oamk.fi>
2021-02-19 11:53:11 +0200

    mutation resolver to login, in order to get bearer token

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,16 +1,20 @@
 const { getRocketsData, getRocketDataById } = require('./rockets')
 const { getMissionsData, getMissionDataById } = require('./missions')
-const { createUser } = require('./user')
+const { createUser, login } = require('./user')
 
 const resolvers = {
   Query: {
+    Me: (root, args, context) => {
+      return context.currentUser
+    },
     getRocket: () => getRocketsData(),
     getRocketDataById: (root, args) => getRocketDataById(args.rocket_id),
     getMissions: () => getMissionsData(),
     getMissionDataById: (root, args) => getMissionDataById(args.mission_id)
   },
   Mutation: {
-    createUser: (root, args) => createUser(args)
+    createUser: (root, args) => createUser(args),
+    login: (root, args) => login(args,)
   }
 }
 
