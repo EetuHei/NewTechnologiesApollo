f9eb886856330acac40e14b9e0fc1fd8c60a0b03
EetuHei <t8heee01@students.oamk.fi>
2021-02-19 11:55:15 +0200

    added comments, and login handler where user gets token if login in was successful

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -1,21 +1,52 @@
+
+const { UserInputError } = require('apollo-server')
 const db = require('../../../db.json')
 const { v4: uuid } = require('uuid')
 const fs = require('fs')
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_SECRET
 
 const createUser = (args) => {
+  // add random id for user
   args.id = String(uuid())
 
+  // if args username is found, throw error
   if(db.user.find(x => x.username === args.username)){
-    return new Error('Username already exists')
+    throw new UserInputError('Username already exists', { invalidArgs: args })
   }
+
+  // add args data into db.users arr
   db.user.push(args)
+
+  // write to file with fs so the user gets saved to the mock database
   fs.writeFile('db.json', JSON.stringify(db), function(err){
-    if(err) return Error('Username already exists')
+    if(err) console.log(err)
   })
+
+  // return added user
   return db.user.find(x => x.username === args.username)
 
 }
 
+const login = (args) => {
+  // find user based on args
+  const user = db.user.find(x => x.username === args.username)
+  // throw error if user is not found or password is wrong
+  if(!user || args.password !== user.password){
+    throw new UserInputError('Wrong credentials')
+  }
+
+  // make obj for token
+  const useForToken = {
+    username: user.username,
+    id: user.id
+  }
+
+  // sign jwt token
+  return { value: jwt.sign(useForToken, secret) }
+}
+
 module.exports = {
-  createUser
+  createUser,
+  login
 }
